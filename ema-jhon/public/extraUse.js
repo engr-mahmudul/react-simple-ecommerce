087a6b1ae2310a713b673f54850f0c2c79c41415
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-27 22:20:38 +0600

    add search field and button

---
+++ ema-jhon/public/extraUse.js
@@ -0,0 +1,27 @@
+
+
+let ay = [
+    {id:1, name:"Musarof Korim", age:40, country:"Bangladesh", laguage:"Bengali", wage:50000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/6.png"},
+    {id:11, name:"Tasnuva Trisha", age:35, country:"Bangladesh", laguage:"Bengali", wage:50000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/11.png"},
+    {id:2, name:"Arfan Nisho", age:30, country:"Bangladesh", laguage:"Bengali", wage:40000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/1.png"},
+    {id:3, name:"Arefin Shuvo", age:30, country:"Bangladesh", laguage:"Bengali", wage:35000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/2.png"},
+    {id:4, name:"Apurba", age:35, country:"Bangladesh", laguage:"Bengali", wage:32000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/3.png"},
+    {id:12, name:"Tanjin Trisha", age:30, country:"Bangladesh", laguage:"Bengali", wage:50000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/12.png"},
+    {id:5, name:"joban", age:34, country:"Bangladesh", laguage:"Bengali", wage:25000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/4.png"},
+    {id:6, name:"Sajol", age:32, country:"Bangladesh", laguage:"Bengali", wage:34000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/5.png"},
+    {id:7, name:"Chanchal Chowdhury", age:40, country:"Bangladesh", laguage:"Bengali", wage:50000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/7.png"},
+    {id:13, name:"Farin", age:28, country:"Bangladesh", laguage:"Bengali", wage:40000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/13.png"},
+    {id:8, name:"Siam Nasir", age:30, country:"Bangladesh", laguage:"Bengali", wage:36000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/8.png"},
+    {id:9, name:"Tousif", age:32, country:"Bangladesh", laguage:"Bengali", wage:30000, 
+    image :"https://github.com/mahmudul-11/dummy-photo/blob/main/9.png"},
+    {id:20, name:"Sabila Nur", age:30, country:"Bangladesh", laguage:"Bengali", wage:50000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/20.png"},
+    {id:10, name:"Tahsan", age:40, country:"Bangladesh", laguage:"Bengali", wage:50000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/2.png"},
+    {id:14, name:"Faria", age:30, country:"Bangladesh", laguage:"Bengali", wage:30000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/2.png"},
+    {id:15, name:"Momo", age:35, country:"Bangladesh", laguage:"Bengali", wage:35000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/15.png"},
+    {id:16, name:"Nadia", age:30, country:"Bangladesh", laguage:"Bengali", wage:25000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/16.png"},
+    {id:17, name:"Mehjabin", age:30, country:"Bangladesh", laguage:"Bengali", wage:40000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/2.png"},
+    {id:18, name:"Sharika", age:30, country:"Bangladesh", laguage:"Bengali", wage:40000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/18.png"},
+  
+    {id:19, name:"Shok", age:34, country:"Bangladesh", laguage:"Bengali", wage:30000, image :"https://github.com/mahmudul-11/dummy-photo/blob/main/2.png"},
+    
+]
--- ema-jhon/src/components/Shop/Shop.js
+++ ema-jhon/src/components/Shop/Shop.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faSearch,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Shop.css'
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
@@ -24,42 +26,51 @@ const Shop = () => {
     // Here data is downloading from the local storage to show in the cart 
     useEffect(() => { // 
         const cartData = getStoredCart() // local storage theke load kora hoccge.
-        
+
         const cartArray = [];
-        if(products.length){ //Check kora hocche fetch korar por data download hoise kina
+        if (products.length) { //Check kora hocche fetch korar por data download hoise kina
             for (const key in cartData) { //cart object er moddhe loop chalanu hocche
                 const matchedData = products.find(product => product.key === key);// products theke cart er  product golake ber kora hocche checking er maddhome.
-                if(matchedData){
-                    const  quantity = cartData[key];
+                if (matchedData) {
+                    const quantity = cartData[key];
                     // console.log(key,quantity);
                     matchedData.quantity = quantity;
                     cartArray.push(matchedData);// noton ekta array te push kora hocche.
-                // console.log(key, matchedData);
+                    // console.log(key, matchedData);
                 }
-                
+
             }
-           setCart(cartArray); // cart state e set koar hocche value
+            setCart(cartArray); // cart state e set koar hocche value
         }
         // console.log(cartArray.length);
-        
+
     }, [products]);// useState e products updata howar por abar useEffet er code ta sobta run hobe. ekhne [] just ekbarei run hobe.egola k depencdecy bole.
 
-    useEffect(()=>{
+    useEffect(() => {
         // console.log(cart);
-    },[cart])
+    }, [cart])
+
+
+    const element1 = <FontAwesomeIcon icon={faSearch} />
     return (
-        <div className='shop-contaienr'>
-            <div className="product-container">
-                {
-                    products.map(product => <Product
-                        key={product.key}
-                        product={product}
-                        addToCart={addToCart}>{/* function props hisebe pathano hocche */}
-                    </Product>)
-                }
+        <div>
+            <div className='search-div'>
+                <input type="text" placeholder="Put here your searching item's name" />
+                <button>{element1}</button>
             </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
+            <div className='shop-contaienr'>
+                <div className="product-container">
+                    {
+                        products.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            addToCart={addToCart}>{/* function props hisebe pathano hocche */}
+                        </Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
             </div>
         </div>
     );
